d617c96acbd0a6cdc7acad50dbc23116668bf372
unknowncoder05 <49960321+unknowncoder05@users.noreply.github.com>
2021-08-06 07:16:41 -0500

    sample blueprints for testing

--- compiler/compiler.py
+++ compiler/compiler.py
@@ -19,3 +19,4 @@ def main():
 
 main()
 # python .\compiler.py c "json.v1" ../blueprints/examples/facebook/main.app.json
+# python .\compiler.py c "json.v1" ./sample_blueprints/samples/import.app.json
--- compiler/json_compiler/Compiler.py
+++ compiler/json_compiler/Compiler.py
@@ -10,32 +10,36 @@ EXTENDS_FIELD = SPECIAL_FIELD_FLAG+"extends"
 def load_json_as_dict(file_name):
     with open(file_name, "r") as f:
         return json.load(f)
-def special_flags_processing(json_dict, args = {}, *, base_folder=None, base_dict={}):
+def special_flags_processing(json_dict, args = {}, *, base_folder=None, base_dict={}, object_route=""):
     if SPECIAL_FIELD_FLAG+"constructor" in json_dict:
         json_dict.pop(SPECIAL_FIELD_FLAG+"constructor")
     if SPECIAL_FIELD_FLAG+"extends" in json_dict:
         extends_from = json_dict[SPECIAL_FIELD_FLAG+"extends"]["from"].split(".")
+        attr_build = {}
         if len(extends_from) == 1:
             if extends_from[0] in base_dict:
-                attr_build = special_flags_processing(base_dict[extends_from[0]], base_dict=base_dict)
-                json_dict.update(attr_build)
+                attr_build = special_flags_processing(base_dict[extends_from[0]], base_dict=base_dict, object_route = object_route+"."+extends_from[0])
             else:
-                raise NameError("ERROR: Attribute not found "+extends_from[0]+"\n"+str(base_dict))
+                raise NameError(f"ERROR: Attribute not found {object_route}.{extends_from[0]}\n{base_dict}")
         else:
             attr_file_name = search_json(json_dict[SPECIAL_FIELD_FLAG+"extends"]["from"], base_folder=base_folder)
             if not attr_file_name:
                 raise NameError(f"ERROR! {json_dict[SPECIAL_FIELD_FLAG+'extends']['from']} path does not exists in")
             attr_json = load_json_as_dict(attr_file_name)
-            attr_build = json_global_compile(attr_json, base_folder = os.path.dirname(attr_file_name))
-            json_dict.update(attr_build)
+            attr_build = json_global_compile(attr_json, base_folder = os.path.dirname(attr_file_name),object_route=json_dict[SPECIAL_FIELD_FLAG+"extends"]["from"])
+        for exclude in json_dict[SPECIAL_FIELD_FLAG+"extends"].get(SPECIAL_FIELD_FLAG+"excludes",[]):
+            if exclude in attr_build:
+                print("EXCLUDES",exclude)
+                attr_build.pop(exclude)
+        json_dict.update(attr_build)
         json_dict.pop(SPECIAL_FIELD_FLAG+"extends")
     for attribute in json_dict:
         if type(json_dict[attribute]) == dict:
-            json_dict[attribute] = special_flags_processing(json_dict[attribute], args, base_folder=base_folder, base_dict=base_dict)
+            json_dict[attribute] = special_flags_processing(json_dict[attribute], args, base_folder=base_folder, base_dict=base_dict, object_route=f"{object_route}.{attribute}")
     return json_dict
 
-def json_global_compile(json_dict, args = {}, *, base_folder=None, base_dict={}):# Should deep copy
-    data = special_flags_processing(json_dict, args, base_folder=base_folder, base_dict=json_dict)
+def json_global_compile(json_dict, args = {}, *, base_folder=None, base_dict={}, object_route= ""):
+    data = special_flags_processing(json_dict, args, base_folder=base_folder, base_dict=json_dict, object_route=object_route)
     return data
 class Compiler:
     blueprint: dict = {}
@@ -49,6 +53,8 @@ class Compiler:
         if MODELS_FIELD not in self.blueprint and EXTENDS_FIELD not in self.blueprint:
             raise NameError("models is not defined")
         build = json_global_compile(self.blueprint)
+        pp = pprint.PrettyPrinter(indent=2)
+        pp.pprint(build)
         for model in build["models"].copy():
             model_file_name = self.main_file
             if type(model) == str:
@@ -61,7 +67,7 @@ class Compiler:
                 model_json = load_json_as_dict(model_file_name)
             elif type(model) == dict:
                 model_json = model
-            model_build = json_global_compile(model_json, base_folder = os.path.dirname(model_file_name))
+            model_build = json_global_compile(model_json, base_folder = os.path.dirname(model_file_name), object_route=model)
             build["models"][model] = model_build
         pp = pprint.PrettyPrinter(indent=2)
         pp.pprint(build)
